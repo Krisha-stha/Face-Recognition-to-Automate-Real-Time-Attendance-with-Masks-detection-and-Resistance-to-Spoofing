a8a40400bf29e8928a4a7e0f5d020f8e027024f8
Krisha Shrestha <skrisha22@tbc.edu.np>
2025-03-30 23:31:21 +0545

    Trained the data

--- train.py
+++ train.py
@@ -4,6 +4,8 @@ from PIL import Image, ImageTk
 from tkinter import messagebox
 import mysql.connector
 import cv2
+import os
+import numpy as np
 
 class Train:
     def __init__(self,root):
@@ -22,7 +24,7 @@ class Train:
         f_lbl.place(x=5,y=55,width=1530,height=325) 
 
         #button
-        button1 = Button(self.root,text="TRAIN DATA", cursor="hand2", font = ("times new roman", 30, "bold"),bg = "white", fg = "pink")
+        button1 = Button(self.root,text="TRAIN DATA", command = self.train_classifier,cursor="hand2", font = ("times new roman", 30, "bold"),bg = "white", fg = "pink")
         button1.place(x=0, y=380, width=1530, height=60)
 
         img_bottom = Image.open(r"my_images\details.jpeg")
@@ -32,9 +34,34 @@ class Train:
         f_lbl = Label(self.root,image=self.photoimg_bottom)
         f_lbl.place(x=5,y=440,width=1530,height=325) 
 
-      
 
+    def train_classifier(self):
+        data_dir =  ("data")
+        path = [os.path.join(data_dir,file) for file in os.listdir(data_dir)]
 
+        faces =[]
+        ids=[]
+
+        for image in path:
+            #Gray scale image
+            img= Image.open(image).convert('L')
+            imageNp = np.array(img, 'uint8')
+            id = int(os.path.split(image)[1].split('.')[1])
+            
+            faces.append(imageNp)
+            ids.append(id)
+
+            cv2.imshow("Training",imageNp)
+            cv2.waitKey(1)==13
+        
+        ids = np.array(ids)
+
+        #train the classifier and save#
+        clf=cv2.face.LBPHFaceRecognizer_create()
+        clf.train(faces,ids)
+        clf.write("classifier.xml")
+        cv2.destroyAllWindows()
+        messagebox.showinfo("Result", "Training dataset completed")
 
 
 
@@ -43,3 +70,6 @@ if __name__ == "__main__":
     root= Tk()
     obj = Train(root)
     root.mainloop()
+
+
+    
